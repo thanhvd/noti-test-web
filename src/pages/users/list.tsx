0f6be680507dfd04e724f889889f79652ed9389e
nson1712 <you@example.com>
2024-02-26 03:07:06 +0700

    feat: create upload file btn

--- src/pages/users/list.tsx
+++ src/pages/users/list.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
 import {
   DateField,
   DeleteButton,
@@ -9,16 +10,57 @@ import {
 } from "@refinedev/antd";
 import { BaseRecord, IResourceComponentsProps } from "@refinedev/core";
 import { Space, Table } from "antd";
-import React from "react";
+import {
+  useApiUrl,
+} from "@refinedev/core";
+import {
+  getValueFromEvent,
+  Create,
+  useForm,
+} from "@refinedev/antd";
+import {
+  Upload,
+  Form,
+  Input,
+} from "antd";
+import { NOTIAPI_URL } from "@/utilities";
+
+interface IPost {
+  id: number;
+  title: string;
+  image: [
+    {
+      uid: string;
+      name: string;
+      url: string;
+      status: "error" | "success" | "done" | "uploading" | "removed";
+    },
+  ];
+}
 
 export const UserList: React.FC<IResourceComponentsProps> = () => {
   const { tableProps } = useTable({
     syncWithLocation: true,
   });
-
+  const { formProps, saveButtonProps } = useForm<IPost>();
 
   return (
+
     <List>
+      <Form>
+        <Form.Item label="File">
+          <Form.Item
+            name="file"
+            valuePropName="fileList"
+            getValueFromEvent={getValueFromEvent}
+            noStyle
+          >
+            <Upload.Dragger name="file" action={`${NOTIAPI_URL}/media/upload`} listType="picture" maxCount={5} multiple>
+              <p className="ant-upload-text">Drag & drop a file in this area</p>
+            </Upload.Dragger>
+          </Form.Item>
+        </Form.Item>
+      </Form>
       <Table {...tableProps} rowKey="id">
         <Table.Column dataIndex="id" title={"ID"} />
         <Table.Column dataIndex="email" title={"Email"} />
