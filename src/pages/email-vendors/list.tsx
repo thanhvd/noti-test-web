bea75f9e5f1cc84ea218baf53acf6704b956cb1e
Thanh Vu <thanh.vu@monkey.edu.vn>
2024-02-28 16:06:42 +0700

    fix: email vendor list

--- src/pages/email-vendors/list.tsx
+++ src/pages/email-vendors/list.tsx
@@ -1,63 +1,54 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NOTIAPI_URL } from "@/utilities";
+import React from 'react';
+import { Space, Switch, Table, notification } from 'antd';
 import {
-  DateField,
   DeleteButton,
   EditButton,
   List,
   ShowButton,
   useTable,
-} from "@refinedev/antd";
-import { BaseRecord, IResourceComponentsProps } from "@refinedev/core";
-import { Select, Space, Switch, Table } from "antd";
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+} from '@refinedev/antd';
+import {
+  BaseRecord,
+  IResourceComponentsProps,
+  useCustomMutation,
+} from '@refinedev/core';
+import { NOTIAPI_URL } from '@/utilities';
 
 export const EmailVendorsList: React.FC<IResourceComponentsProps> = () => {
-  const { tableProps } = useTable({
+  const { tableProps, tableQueryResult } = useTable({
     syncWithLocation: true,
   });
+  const { mutate, isLoading } = useCustomMutation();
 
-  console.log("TB: ", tableProps.dataSource)
-
-  const [switchStates, setSwitchStates] = useState<{ [key: string]: boolean }>(() => {
-    const savedState = localStorage.getItem("switchStates");
-    return savedState ? JSON.parse(savedState) : {};
-  });
-  const [dataSource, setDataSource] = useState<BaseRecord[]>([]);
-  const handleChangeStatus = (checked: boolean, id: any) => {
-    axios({
-      method: "put",
-      url: `${NOTIAPI_URL}/email/vendor/${id}/update`,
-      data: {
-        status: checked === true ? "ACTIVE" : "INACTIVE"
+  const handleChangeStatus = async (checked: boolean, id: any) => {
+    await mutate(
+      {
+        method: 'put',
+        url: `${NOTIAPI_URL}/email/vendor/${id}/update`,
+        values: {
+          status: checked === true ? 'ACTIVE' : 'INACTIVE',
+        },
+      },
+      {
+        onError: (error, variables, context) => {
+          console.log('ERROR');
+        },
+        onSuccess: (data, variables, context) => {
+          notification['success']({
+            message: 'Successful',
+            description: 'Successfully updated email vendor status',
+          });
+          tableQueryResult.refetch();
+        },
       }
-    }).then(() => {
-      setSwitchStates(prevStates => ({
-        ...prevStates,
-        [id]: checked
-      }));
-
-    });
+    );
   };
 
-  useEffect(() => {
-    if (tableProps?.dataSource) {
-      const updatedDataSource = tableProps.dataSource.map((record: BaseRecord) => ({
-        ...record,
-        status: switchStates[record.id ?? ''] ? "ACTIVE" : "INACTIVE"
-      }));
-      setDataSource(updatedDataSource);
-    }
-  }, [tableProps.dataSource, switchStates]);
-
-  useEffect(() => {
-    localStorage.setItem("switchStates", JSON.stringify(switchStates));
-  }, [switchStates]);
   return (
     <List>
-      <Table {...tableProps} dataSource={dataSource} rowKey="id" key={JSON.stringify(dataSource)}>
-        <Table.Column dataIndex="id" title={"ID"} />
+      <Table {...tableProps} dataSource={tableProps?.dataSource} rowKey='id'>
+        <Table.Column dataIndex='id' title={'ID'} />
         {/* <Table.Column
           dataIndex={["createdAt"]}
           title={"Created At"}
@@ -73,26 +64,32 @@ export const EmailVendorsList: React.FC<IResourceComponentsProps> = () => {
           title={"Updated At"}
           render={(value: any) => <DateField value={value} />}
         /> */}
-        <Table.Column dataIndex="hostName" title={"Hostname"} />
-        <Table.Column dataIndex="port" title={"Port"} />
-        <Table.Column dataIndex="userName" title={"Username"} />
+        <Table.Column dataIndex='hostName' title={'Hostname'} />
+        <Table.Column dataIndex='port' title={'Port'} />
+        <Table.Column dataIndex='userName' title={'Username'} />
         {/* <Table.Column dataIndex="password" title={"Password"} /> */}
-        <Table.Column dataIndex="priority" title={"Priority"} />
-        <Table.Column dataIndex="status" title={"Status"} />
+        <Table.Column dataIndex='priority' title={'Priority'} />
+        <Table.Column
+          dataIndex='status'
+          title={'Status'}
+          render={(_, record: BaseRecord) => (
+            <Switch
+              disabled={isLoading || tableQueryResult.isLoading}
+              loading={isLoading || tableQueryResult.isLoading}
+              checked={record.status === 'ACTIVE'}
+              onChange={(checked) => handleChangeStatus(checked, record.id)}
+            />
+          )}
+        />
 
         <Table.Column
-          title={"Actions"}
-          dataIndex="actions"
+          title={'Actions'}
+          dataIndex='actions'
           render={(_, record: BaseRecord) => (
             <Space>
-              <Switch
-                checked={switchStates[record.id ?? '']}
-                onChange={(checked) => handleChangeStatus(checked, record.id)}
-              />
-              <EditButton hideText size="small" recordItemId={record.id} />
-              <ShowButton hideText size="small" recordItemId={record.id} />
-              <DeleteButton hideText size="small" recordItemId={record.id} />
-
+              <EditButton hideText size='small' recordItemId={record.id} />
+              <ShowButton hideText size='small' recordItemId={record.id} />
+              <DeleteButton hideText size='small' recordItemId={record.id} />
             </Space>
           )}
         />
